cf3cdb19b75f1202964738bdcf3143939c126bcc
Vanya1000 <Vanya1000@yandex.ru>
2021-11-09 21:30:55 +0300

    21_30

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 "use strict"
+const randomLoc = Math.floor(Math.random() * 5);
+const location1 = randomLoc;
+const location2 = location1 + 1;
+const location3 = location2 + 1;
+let guess;
+let hits = 0;
+let guesses = 0;
+var isSunk = false;
+while (isSunk == false) {
+	guess = prompt('Приготовьтесь, огонь!(Введите цифру от 0 до 6)');
+	if (guess < 0 || guess > 6) {
+		alert('Ты что творишь? ');
+	} else {
+		guesses = guesses + 1;
+		if (guess == location1 || guess == location2 || guess == location3) {
+			alert('Попал');
+			hits = hits + 1;
+			if (hits == 3) {
+				isSunk = true;
+				alert('А ты ниче так!')
+			}
+		} else {
+			alert('Мимо');
+		}
+	}
+}
+const finish = document.querySelector('.textFinish');
+finish.style.display='block';
+let stats = `Ты использовала ${guesses} попыток и потопила таки корабль! Точность попадания составила(${3/guesses})`
+alert(stats);
+
+
+
+
