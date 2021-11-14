e5580054dcfa473d5d690b122b7f543ddb77ff84
Vanya1000 <Vanya1000@yandex.ru>
2021-11-14 20:58:15 +0300

    20_58

--- js/script.js
+++ js/script.js
@@ -1,32 +1,108 @@
 "use strict"
-/*const randomLoc = Math.floor(Math.random() * 5);
-const location1 = randomLoc;
-const location2 = location1 + 1;
-const location3 = location2 + 1;
-let guess;
-let hits = 0;
-let guesses = 0;
-var isSunk = false;
-while (isSunk == false) {
-	guess = prompt('Приготовьтесь, огонь!(Введите цифру от 0 до 6)');
-	if (guess < 0 || guess > 6) {
-		alert('Ты что творишь? ');
-	} else {
-		guesses = guesses + 1;
-		if (guess == location1 || guess == location2 || guess == location3) {
-			alert('Попал');
-			hits = hits + 1;
-			if (hits == 3) {
-				isSunk = true;
-				alert('А ты ниче так!')
+let view = {
+	displayMessage: function(msq) {
+		let messageArea = document.querySelector("#messageArea");
+		messageArea.innerHTML = msq;
+	},
+	displayHit: function(location) {
+		let cell = document.getElementById(location);
+		cell.classList.add("hit"); 
+	},
+	displayMiss: function(location) {
+		let cell = document.getElementById(location);
+		cell.classList.add("miss"); 
+	}
+};
+
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+	
+	ships: [
+		{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+		{ locations: ["24", "34", "44"], hits: ["", "", ""] },
+		{ locations: ["10", "11", "12"], hits: ["", "", ""] }
+	],
+	fire: function (guess) {
+		for (let i = 0; i < this.numShips; i++) {
+			let ship = this.ships[i];
+			let index = ship.locations.indexOf(guess);
+			if (index >= 0) {
+				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("Попали!");
+				if (this.isSunk(ship)) {
+					view.displayMessage("Вы потопили корабль!");
+					this.shipsSunk++;
+				}
+				return true;
 			}
-		} else {
-			alert('Мимо');
 		}
+		view.displayMiss(guess);
+		view.displayMessage("Вы промахнулись!");
+		return false;
+	},
+	isSunk: function(ship) {
+		for (let i = 0; i < this.shipLength; i++) {
+			if (ship.hits[i] !== "hit") {
+				return false;
+			}
+		} 
+		return true;
+	},
+};
+let controller = {
+	guesses: 0,
+	processGuess: function(guess) {
+		let location  = parseGuess(guess);
+		if (location) {
+			this.guesses++;
+			let hit = model.fire(location);
+			if (hit & model.shipsSunk === model.numShips) {
+				view.displayMessage(`Вы потопили все корабли! Затратив на это ${this.guesses} выстрелов.`);
+				table.removeEventListener("click", touch);
+			}
+		}
+	},
+	
+};
+
+function parseGuess(guess) {
+	const alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+	if (guess.match(/[A-G][0-6]/)) {
+		let firstChar = guess.charAt(0);
+		let row = alphabet.indexOf(firstChar)
+		let column = guess.charAt(1);
+		return row + column;
+	} else if (guess.match(/[0-6][0-6]/)) { //для тач
+		return guess;
+  } else {
+    view.displayMessage("Введите корректное значение!");
+  }
+};
+
+function init() {
+	let fireButton = document.querySelector("#fireButton");
+	fireButton.onclick = handleFireButton;
+};
+
+function handleFireButton() {
+	let guessInput = document.querySelector("#guessInput");
+	let guess = guessInput.value;
+	controller.processGuess(guess);
+	guessInput.value = "";
+};
+window.onload = init;
+
+
+function touch () {
+	let target = event.target;
+	if (target.closest('td')) {
+		console.log(target.getAttribute('id'));
+		controller.processGuess(target.getAttribute("id"));
 	}
 }
-const finish = document.querySelector('.textFinish');
-finish.style.display='block';
-let stats = `Ты использовала ${guesses} попыток и потопила таки корабль! Точность попадания составила(${3/guesses})`
-alert(stats);
-*/
+const table = document.querySelector("table");
+table.addEventListener('click', touch);
